228fdc672a0c429349c1b396cf47a806510993ee
Maziarz <maziarz.maciej@gmail.com>
2016-11-18 19:44:38 +0100

    Skeleton image

--- MegaGra/Monster.cs
+++ MegaGra/Monster.cs
@@ -7,15 +7,29 @@ using System.Windows.Forms;
 
 namespace MegaGra
 {
-    class Monster
+    class Monster : IDamageReceiver, IDamageDealer
     {
         public int healthPoints { get; set; }
         public int attack { get; set; }
-        public int def { get; set; }
+        public int defense { get; set; }
         public int level { get; set; }
         public int exp { get; set; }
         public ProgressBar healthBar { get; set; }
         public PictureBox monsterimage { get; set; }
+
+        void IDamageReceiver.takeDamage(int dmg)
+        {
+            this.healthPoints -= (dmg * defense / 2);
+            this.healthBar.Value = this.healthPoints;
+        }
+
+        void IDamageDealer.dealDamage(IDamageReceiver enemy)
+        {
+            int totalDamage = attack * level;
+            enemy.takeDamage(totalDamage); 
+
+        }
+
         
 
         
--- MegaGra/Skeleton.cs
+++ MegaGra/Skeleton.cs
@@ -9,21 +9,37 @@ namespace MegaGra
 {
     class Skeleton : Monster
     {
+       
+       
 
-        public Skeleton (Form form, int healthPoints = 100, int attack = 10, int def = 1, int level = 1, int exp = 50)
+        public Skeleton (Form form, int healthPoints = 100, int attack = 10, int defense = 1, int level = 1, int exp = 50)
         {
             this.healthPoints = healthPoints;
             this.attack = attack;
-            this.def = def;
+            this.defense= defense;
             this.level = level;
-            this.exp = exp;
-
+            this.exp = exp;     
+            
             this.healthBar = new ProgressBar();
             this.healthBar.Parent = form;          
             this.healthBar.Location = new System.Drawing.Point(10, 40);
             this.healthBar.Show();
             this.healthBar.Maximum = healthPoints;
-            this.healthBar.Value = 70;
+            this.healthBar.Value = healthPoints;
+
+            PictureBox img = new PictureBox();
+            img.Parent = form;            
+            img.Location = new System.Drawing.Point(10, 50);
+            img.Width = 100;
+            img.Height = 100;
+            img.SizeMode = PictureBoxSizeMode.StretchImage;
+            img.ImageLocation = @"C:\Projekty\c#\Macki2\MegaGra\resources\skeleton.jpg";
+            img.Show();
+
+
+
+
+
 
         }
 
