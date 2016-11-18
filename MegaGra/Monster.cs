6e5c65e9941421c7a84eb4c77192699c5cbebce0
Maziarz <maziarz.maciej@gmail.com>
2016-11-18 17:43:51 +0100

    nowe rzeczy

--- MegaGra/Monster.cs
+++ MegaGra/Monster.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MegaGra
 {
     class Monster
     {
-        int hp;
+        int healthPoints;
+        int attack;
+        int def;
+        int level;
+        int exp;
+        ProgressBar healthBar;
+        PictureBox monsterimage;
+
+        
+        
+
+        
+        
     }
 }
